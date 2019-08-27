97d5ef5fe3bf84f16813ebdffb6f198b7b81454c
Kevin <kevinschoon@gmail.com>
2019-08-27 16:15:04 -0500

    output projects as tree

--- cmd_get.go
+++ cmd_get.go
@@ -2,43 +2,85 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
-	"sort"
+	// "sort"
 
 	cli "github.com/jawher/mow.cli"
 )
 
+type Kind string
+
+const (
+	PROJECT  = "projects"
+	TASK     = "tasks"
+	POMODORO = "pomodoros"
+)
+
 func get(config *Config) func(*cli.Cmd) {
 	return func(cmd *cli.Cmd) {
-		cmd.Spec = "[OPTIONS]"
+		cmd.Spec = "[OPTIONS] [KIND] [ID]"
 		var (
-			ascend = cmd.BoolOpt("a ascend", false, "sort tasks in ascending order")
-			limit  = cmd.IntOpt("l limit", 0, "limit returned tasks")
-			asJson = cmd.BoolOpt("json", false, "write as json")
+			kind = cmd.StringArg("KIND", PROJECT, "project | task | pomodoro")
+			id   = cmd.IntArg("ID", 0, "resource identifier")
+			// ascend = cmd.BoolOpt("a ascend", false, "sort tasks in ascending order")
+			// limit  = cmd.IntOpt("l limit", 0, "limit returned tasks")
+			asJson = cmd.BoolOpt("json", false, "output result as JSON")
 		)
 		cmd.Action = func() {
 			store, err := NewSQLiteStore(config.DBPath)
 			maybe(err)
 			defer store.Close()
-			var tasks []*Task
-			maybe(store.With(func(s Store) error {
-				t, err := s.ReadTasks(-1)
-				if err != nil {
-					return err
+			switch *kind {
+			case PROJECT:
+				var project *Project
+				maybe(store.With(func(s Store) error {
+					root := &Project{Title: "root"}
+					rootTasks, err := s.ReadTasks(int64(0))
+					if err != nil {
+						return err
+					}
+					root.Tasks = rootTasks
+					projects, err := s.ReadProjects(int64(*id))
+					if err != nil {
+						return err
+					}
+					root.Children = projects
+					project = root
+					return nil
+				}))
+				if *asJson {
+					maybe(json.NewEncoder(os.Stdout).Encode(project))
+					return
 				}
-				tasks = t
-				return nil
-			}))
-			if *ascend {
-				sort.Sort(sort.Reverse(ByID(tasks)))
-			}
-			if *limit > 0 && (len(tasks) > *limit) {
-				tasks = tasks[0:*limit]
-			}
-			if *asJson {
-				maybe(json.NewEncoder(os.Stdout).Encode(tasks))
-			} else {
-				summerizeTasks(config, tasks)
+				Tree(*project).Write(os.Stdout, 0, len(project.Children) == 0)
+			case TASK:
+				var tasks []*Task
+				maybe(store.With(func(s Store) error {
+					if *id == 0 {
+						result, err := s.ReadTasks(int64(*id))
+						if err != nil {
+							return err
+						}
+						tasks = result
+						return err
+					} else {
+						task := &Task{
+							ID: int64(*id),
+						}
+						err := s.ReadTask(task)
+						if err != nil {
+							return err
+						}
+						tasks = []*Task{task}
+						return nil
+					}
+				}))
+				if *asJson {
+					maybe(json.NewEncoder(os.Stdout).Encode(tasks))
+				}
+			default:
+				maybe(fmt.Errorf("unknown resource: %s", *kind))
 			}
 		}
 	}
--- project.go
+++ project.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"time"
 )
 
@@ -24,4 +26,52 @@ func (p Project) Duration() time.Duration {
 		duration += task.Duration * time.Duration(int64(len(task.Pomodoros)))
 	}
 	return duration
+}
+
+const (
+	emptySpace   = "    "
+	middleItem   = "├── "
+	continueItem = "│   "
+	lastItem     = "└── "
+)
+
+type Tree Project
+
+func (t Tree) Write(w io.Writer, depth int, last bool) {
+	if depth == 0 { // root
+		fmt.Fprintf(w, ".\n")
+	}
+	spaces := depth
+	for i, task := range t.Tasks {
+		for j := 0; j < spaces; j++ {
+			if j == 0 && !last {
+				fmt.Fprintf(w, continueItem)
+				continue
+			}
+			fmt.Fprintf(w, emptySpace)
+		}
+		if i+1 == len(t.Tasks) && len(t.Children) == 0 {
+			fmt.Fprintf(w, lastItem)
+		} else {
+			fmt.Fprintf(w, middleItem)
+		}
+		fmt.Fprintf(w, "[T][%d] %s \n", task.ID, task.Message)
+	}
+
+	for n, child := range t.Children {
+		for j := 0; j < spaces; j++ {
+			if j == 0 && !last {
+				fmt.Fprintf(w, continueItem)
+				continue
+			}
+			fmt.Fprintf(w, emptySpace)
+		}
+		if n+1 == len(t.Children) {
+			fmt.Fprintf(w, lastItem)
+		} else {
+			fmt.Fprintf(w, middleItem)
+		}
+		fmt.Fprintf(w, "[P][%d] %s\n", child.ID, child.Title)
+		Tree(*child).Write(w, depth+1, n+1 == len(t.Children) && depth == 0)
+	}
 }
