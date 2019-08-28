5093abfbae38f9c11262ba8350d7acbb06a16229
Kevin <kevinschoon@gmail.com>
2019-08-28 13:49:45 -0500

    rm dead code

--- util.go
+++ util.go
@@ -6,9 +6,6 @@ import (
 	"os"
 	"os/user"
 	"path"
-	"time"
-
-	"github.com/fatih/color"
 )
 
 func maybe(err error) {
@@ -41,63 +38,6 @@ func makeTimers(task Task) []*Timer {
 		timers[i] = NewTimer(task.Duration, runtime, pauseTime)
 	}
 	return timers
-}
-
-// TODO: Make more configurable / possibly remove all together
-func summerizeTasks(config *Config, tasks []*Task) {
-	for _, task := range tasks {
-		var start string
-		if len(task.Pomodoros) > 0 {
-			start = task.Pomodoros[0].Start.Format(config.DateTimeFmt)
-		}
-		fmt.Printf("%d: [%s] [%s] ", task.ID, start, task.Duration.Truncate(time.Second))
-		// a list of green/yellow/red pomodoros
-		// green indicates the pomodoro was finished normally
-		// yellow indicates the break was exceeded by +5minutes
-		// red indicates the pomodoro was never completed
-		fmt.Printf("[")
-		for i, pomodoro := range task.Pomodoros {
-			if i > 0 {
-				fmt.Printf(" ")
-			}
-			// pomdoro was paused more than 5m
-			if pomodoro.PauseTime > task.Duration+5*time.Minute {
-				color.New(color.FgYellow).Printf("X")
-			} else {
-				// pomodoro completed normally
-				color.New(color.FgGreen).Printf("X")
-			}
-		}
-		// each missed pomodoro
-		/*
-			for i := 0; i < task.NPomodoros-len(task.Pomodoros); i++ {
-				if i > 0 || i == 0 && len(task.Pomodoros) > 0 {
-					fmt.Printf(" ")
-				}
-				color.New(color.FgRed).Printf("X")
-			}
-		*/
-		fmt.Printf("]")
-		// Tags
-		if len(task.Tags) > 0 {
-			fmt.Printf(" [")
-			for i, tag := range task.Tags {
-				if i > 0 && i != len(task.Tags) {
-					fmt.Printf(" ")
-				}
-				// user specified color mapping exists
-				if color := config.Colors.Get(tag); color != nil {
-					color.Printf("%s", tag)
-				} else {
-					// no color mapping
-					fmt.Printf("%s", tag)
-				}
-			}
-			fmt.Printf("]")
-		}
-		fmt.Printf(" - %s", task.Message)
-		fmt.Printf("\n")
-	}
 }
 
 func outputStatus(status Status) {
