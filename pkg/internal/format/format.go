fc52bed96da3de1ec5bac88f1b47ea664da1cd6a
Kevin <kevinschoon@gmail.com>
2019-09-05 17:32:22 -0500

    fix TruncDuration

--- pkg/internal/format/format.go
+++ pkg/internal/format/format.go
@@ -6,11 +6,12 @@ import (
 )
 
 func TruncDuration(duration time.Duration) string {
-	duration = duration.Round(time.Minute)
 	if duration >= time.Hour {
+		duration = duration.Round(time.Minute)
 		return fmt.Sprintf("%02dh%02dm", duration/time.Hour, (duration-(duration/time.Hour)*time.Hour)/time.Minute)
 	} else if duration >= time.Minute {
+		duration = duration.Round(time.Minute)
 		return fmt.Sprintf("%02dm", duration/time.Minute)
 	}
-	return duration.Round(time.Minute).String()
+	return duration.Round(time.Second).String()
 }
