98ac578fd84b884fc8cbdb5f2421d30dc1ad67d2
Kevin Schoon <kevinschoon@gmail.com>
2019-10-19 20:03:10 -0400

    fix truncate

--- pkg/internal/format/format.go
+++ pkg/internal/format/format.go
@@ -5,13 +5,22 @@ import (
 	"time"
 )
 
+const day = (24 * time.Hour)
+
 func TruncDuration(duration time.Duration) string {
-	if duration >= time.Hour {
-		duration = duration.Round(time.Minute)
-		return fmt.Sprintf("%02dh%02dm", duration/time.Hour, (duration-(duration/time.Hour)*time.Hour)/time.Minute)
-	} else if duration >= time.Minute {
-		duration = duration.Round(time.Minute)
+	switch {
+	case duration <= time.Minute:
+		return "0m"
+	case duration <= time.Hour:
 		return fmt.Sprintf("%02dm", duration/time.Minute)
+	case duration <= (24 * time.Hour):
+		nHours := duration / time.Hour
+		nMinutes := (duration - (nHours * time.Hour)) / time.Minute
+		return fmt.Sprintf("%dh%dm", nHours, nMinutes)
+	default:
+		nDays := (duration / day)
+		nHours := ((duration - (nDays * day)) / time.Hour)
+		nMinutes := ((duration - ((nDays * day) + (nHours * time.Hour))) / time.Minute)
+		return fmt.Sprintf("%dd%dh%dm", nDays, nHours, nMinutes)
 	}
-	return duration.Round(time.Second).String()
 }
